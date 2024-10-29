e370063e3bbaa1020cecbe148ae207e52aa11db2
Shin Liu <ucfusl6@ucl.ac.uk>
2024-10-29 00:27:09 +0000

    Homework code

--- wk4_Prac1.R
+++ wk4_Prac1.R
@@ -26,7 +26,26 @@ install.packages("gitcreds")
 library(gitcreds)
 
 gitcreds_set()
-0
+
+## Use 4.5.6 If have an existing project - way 3
+
+## 4.5.8 Push to Github
+use_github()
+1
+
+
+
+## 4.6 RMarkdown
+## >> File > New File > R Markdown
+
+
+
+## 4.6.1 HTML
+## We are now going to insert some code from the practical last week into the new R Markdown document …clear all of the code except the stuff between the —
+## In RStudio, you can either select Code > Insert Chunk or you can Click the ‘Insert’ button and insert an R chunk
+
+
+
 
 
 
